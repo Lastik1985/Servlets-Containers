43da880c0bdd36dcc5c7b5a392f90593147e2297
Lastik1985 <Lastik1985@mail.ru>
2023-06-23 03:15:02 +0300

    feature/di-annotation

--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -16,7 +16,9 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-      final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+      final var context = new AnnotationConfigApplicationContext("ru.netology.controller",
+              "ru.netology.repository", "ru.netology.service");
+
       controller = context.getBean(PostController.class);
     }
 
