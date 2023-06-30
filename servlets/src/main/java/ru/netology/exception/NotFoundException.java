13e84190f1437ee16206584ea15e2edfeb1f6c93
Lastik1985 <Lastik1985@mail.ru>
2023-06-30 13:01:02 +0300

    Spring

--- servlets/src/main/java/ru/netology/exception/NotFoundException.java
+++ servlets/src/main/java/ru/netology/exception/NotFoundException.java
@@ -1,5 +1,10 @@
 package ru.netology.exception;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ResponseStatus(code = NOT_FOUND)
 public class NotFoundException extends RuntimeException {
   public NotFoundException() {
   }
--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,27 +1,45 @@
 package ru.netology.repository;
 
-import org.springframework.stereotype.Repository;
+
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
-@Repository
 public class PostRepository {
+  private final ConcurrentHashMap<Long, Post> postsRepository = new ConcurrentHashMap<>();
+  static AtomicLong countId = new AtomicLong(1);
+
   public List<Post> all() {
-    return Collections.emptyList();
+    return new ArrayList<>(postsRepository.values());
   }
 
   public Optional<Post> getById(long id) {
-    return Optional.empty();
+    return Optional.ofNullable(postsRepository.get(id));
   }
 
-  public Post save(Post post) {
+  public Post save(Post post) throws NotFoundException {
+
+    if (post.getId() == 0) {
+      post.setId(countId.get());
+      countId.getAndIncrement();
+
+    } else if (postsRepository.containsKey(post.getId())) {
+      postsRepository.put(post.getId(), post);
+    } else throw new NotFoundException("Такого поста нет");
     return post;
   }
 
-  public void removeById(long id) {
+  public void removeById(long id) throws NotFoundException {
+    if (postsRepository.containsKey(id)) {
+      postsRepository.remove(id);
+    } else {
+      throw new NotFoundException("Такого поста нет");
+    }
   }
 }
